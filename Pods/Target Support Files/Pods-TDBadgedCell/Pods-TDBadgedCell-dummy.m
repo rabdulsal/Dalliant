2d82f5c011035e0eb3de438a8eb57f96c2669c18
Rashad Abdul-Salaam <rashad@getskinclick.com>
2014-12-04 20:58:38 -0600

    Adding TDBadgeCell Pod

---
+++ Pods/Target Support Files/Pods-TDBadgedCell/Pods-TDBadgedCell-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TDBadgedCell : NSObject
+@end
+@implementation PodsDummy_Pods_TDBadgedCell
+@end
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// TDBadgedCell
+#define COCOAPODS_POD_AVAILABLE_TDBadgedCell
+#define COCOAPODS_VERSION_MAJOR_TDBadgedCell 2
+#define COCOAPODS_VERSION_MINOR_TDBadgedCell 6
+#define COCOAPODS_VERSION_PATCH_TDBadgedCell 0
+
