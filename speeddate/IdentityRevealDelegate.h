d3b978c617ca2b49bcd21685c9e13fd91d284ec6
Rashad Abdul-Salaam <rashad@getskinclick.com>
2015-09-23 22:39:59 -0500

    Re-add Identity Protocol

--- speeddate/IdentityRevealDelegate.h
+++ speeddate/IdentityRevealDelegate.h
@@ -1,13 +1,12 @@
 //
-//  IdentityRevealProtocol.h
+//  IdentityRevealDelegate.h
 //  speeddate
 //
-//  Created by Rashad Abdul-Salaam on 9/20/15.
+//  Created by Rashad Abdul-Salaam on 9/23/15.
 //  Copyright (c) 2015 Studio76. All rights reserved.
 //
 
-#ifndef speeddate_IdentityRevealProtocol_h
-#define speeddate_IdentityRevealProtocol_h
+#import <Foundation/Foundation.h>
 
 @protocol IdentityRevealDelegate <NSObject>
 
@@ -16,5 +15,3 @@
 - (void)revealRequestRejected;
 
 @end
-
-#endif
